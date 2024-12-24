832f9409ff2084ae453c5f3f3624134d81206ff4
Vansh <vanshsaini4499@gmail.com>
2024-12-24 12:55:48 +0530

    recent tabs modification left

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -10,16 +10,42 @@ const ContextProvider = (props) => {
   const [showResult, setShowResult] = useState(false);
   const [loading, setLoading] = useState(false);
   const [resultData, setResultData] = useState("");
+  const delayPara=(index,nextWord)=>{
+   setTimeout(()=>{setResultData (prev=>prev+nextWord)},index*75)
+  }
 
   const onSent = async (prompt) => {
     setResultData("")
     setLoading(true)
     setShowResult(true)
-    setRecentPrompt(input);
+    let response;
+    if(prompt!==undefined){
+      response=await run(prompt);
+      recentPrompts(prompt);
 
+    }else{
+      setPreviousPrompts([...prevPrompts,input])
+      setRecentPrompt(input)
+      response=await run(input);
 
-    const response =await run(input);
-    setResultData(response);
+    }
+    
+    let responseArray = response.split("**");
+    let newResponse="";
+    for(let i=0;i<responseArray.length;i++){
+      if (i % 2 !== 0) {
+        // Bold sections
+        newResponse += `<b>${responseArray[i]}</b>`;
+      } else {
+        // Regular text
+        newResponse += responseArray[i];
+      }
+    } 
+    let newResponse2=newResponse.split("*").join("<br>")
+    let newResponseArray=newResponse2.split(" ");
+    for(let i=0;i<newResponseArray.length;i++){
+      delayPara(i,newResponseArray[i]+" ")
+    }
     setLoading(false);
     setInput('')
     // Log the response
